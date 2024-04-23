cefe0e5fd911c6a1284acf107ce85d523217c5b4
Tatiana Pastel <pastel.t@yandex.ru>
2024-04-23 15:39:42 +0400

    added test

--- src/test/java/ru/course/taskfive/TaskFiveApplicationTests.java
+++ src/test/java/ru/course/taskfive/TaskFiveApplicationTests.java
@@ -49,25 +49,86 @@ class TaskFiveApplicationTests {
 		RestAssured.baseURI = "http://localhost:" + port;
 		r.deleteAll();
 	}
-
 	@Test
 	@Sql({"/schema.sql"})
-	void registerServiceTest() {
+	void productServiceTest() {
 		given()
 				.contentType(ContentType.JSON)
 				.when()
 				.body("{\n" +
-						"    \"instanceId\": 2,\n" +
-						"    \"registryTypeCode\": \"555111\",\n" +
-						"    \"currencyCode\":\"643\",\n" +
-						"    \"branchCode\":\"777\",\n" +
-						"    \"priorityCode\":\"00\",\n" +
-						"    \"mdmCode\":\"245020\"\n" +
+						"  \"instanceId\": null,\n" +
+						"  \"productType\": \"Депозит\",\n" +
+						"  \"productCode\": \"211\",\n" +
+						"  \"registerType\": \"555111\",\n" +
+						"  \"mdmCode\": \"100100001\",\n" +
+						"  \"contractNumber\": \"3884\",\n" +
+						"  \"priority\": 2,\n" +
+						"  \"contractDate\": \"2024-02-10\",\n" +
+						"  \"interestRatePenalty\": \"1.5\",\n" +
+						"  \"minimalBalance\": \"1000\",\n" +
+						"  \"thresholdAmount\": \"1000\",\n" +
+						"  \"accountingDetails\": \"details\",\n" +
+						"  \"rateType\": \"дифференцированная\",\n" +
+						"  \"taxPercentageRate\": \"10\",\n" +
+						"  \"technicalOverdraftLimitAmount\": \"3000\",\n" +
+						"  \"contractId\": 1,\n" +
+						"  \"branchCode\": \"777\",\n" +
+						"  \"isoCurrencyCode\": \"643\",\n" +
+						"  \"urgencyCode\": \"00\",\n" +
+						"  \"referenceCode\": 3819,\n" +
+						"  \"instanceArrangement\": [\n" +
+						"    {\n" +
+						"      \"generalAgreementId\": \"154466\",\n" +
+						"      \"supplementaryAgreementId\": \"779951\",\n" +
+						"      \"arrangementType\": \"НСО\",\n" +
+						"      \"shedulerJobId\": 4,\n" +
+						"      \"number\": \"116\",\n" +
+						"      \"openingDate\": \"2024-04-04\",\n" +
+						"      \"closingDate\": \"2025-04-04\",\n" +
+						"      \"cancelDate\": \"2025-04-04\",\n" +
+						"      \"validityDuration\": 1,\n" +
+						"      \"cancellationReason\": \"reason\",\n" +
+						"      \"status\": \"открыт\",\n" +
+						"      \"interestCalculationDate\": \"2024-04-04\",\n" +
+						"      \"interestRate\": \"1.05\",\n" +
+						"      \"coefficient\": \"11\",\n" +
+						"      \"coefficientAction\": \"1\",\n" +
+						"      \"minimumInterestRate\": \"1\",\n" +
+						"      \"minimumInterestRateCoefficient\": \"1\",\n" +
+						"      \"minimumInterestRateCoefficientAction\": \"повышающий\",\n" +
+						"      \"maximalnterestRate\": \"2.5\",\n" +
+						"      \"maximalnterestRateCoefficient\": \"5\",\n" +
+						"      \"maximalnterestRateCoefficientAction\": \"повышающий\"\n" +
+						"    },\n" +
+						"    {\n" +
+						"      \"generalAgreementId\": \"154466\",\n" +
+						"      \"supplementaryAgreementId\": \"779951\",\n" +
+						"      \"arrangementType\": \"НСО\",\n" +
+						"      \"shedulerJobId\": 4,\n" +
+						"      \"number\": \"9999\",\n" +
+						"      \"openingDate\": \"2024-04-04\",\n" +
+						"      \"closingDate\": \"2025-04-04\",\n" +
+						"      \"cancelDate\": \"2025-04-04\",\n" +
+						"      \"validityDuration\": 1,\n" +
+						"      \"cancellationReason\": \"reason\",\n" +
+						"      \"status\": \"открыт\",\n" +
+						"      \"interestCalculationDate\": \"2024-04-04\",\n" +
+						"      \"interestRate\": \"1.05\",\n" +
+						"      \"coefficient\": \"11\",\n" +
+						"      \"coefficientAction\": \"1\",\n" +
+						"      \"minimumInterestRate\": \"1\",\n" +
+						"      \"minimumInterestRateCoefficient\": \"1\",\n" +
+						"      \"minimumInterestRateCoefficientAction\": \"повышающий\",\n" +
+						"      \"maximalnterestRate\": \"2.5\",\n" +
+						"      \"maximalnterestRateCoefficient\": \"5\",\n" +
+						"      \"maximalnterestRateCoefficientAction\": \"повышающий\"\n" +
+						"    }\n" +
+						"  ]\n" +
 						"}")
-				.post("/api/v1/corporate-settlement-account/create")
+				.post("/api/v1/corporate-settlement-instance/create")
 				.then()
 				.statusCode(200);
 
-		Assertions.assertEquals(1, r.count()); // register
+		Assertions.assertEquals(2, a.count()); // agreement
 	}
 }
